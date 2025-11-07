47e52f2684760598b1d1884e7b60494e77f4ba31
12138 <48377190+wqreytuk@users.noreply.github.com>
2025-11-07 18:27:55 +0800

    Create win11 object index.b.c

---
+++ win11 object index.b.c
@@ -0,0 +1,219 @@
+succeed to call PhEnumObjectTypes
+TypeName: Type
+TypeIndex: 2
+
+TypeName: Directory
+TypeIndex: 3
+
+TypeName: SymbolicLink
+TypeIndex: 4
+
+TypeName: Token
+TypeIndex: 5
+
+TypeName: Session
+TypeIndex: 6
+
+TypeName: Job
+TypeIndex: 7
+
+TypeName: Process
+TypeIndex: 8
+
+TypeName: Thread
+TypeIndex: 9
+
+TypeName: Partition
+TypeIndex: 10
+
+TypeName: UserApcReserve
+TypeIndex: 11
+
+TypeName: IoCompletionReserve
+TypeIndex: 12
+
+TypeName: ActivityReference
+TypeIndex: 13
+
+TypeName: ProcessStateChange
+TypeIndex: 14
+
+TypeName: ThreadStateChange
+TypeIndex: 15
+
+TypeName: CpuPartition
+TypeIndex: 16
+
+TypeName: SchedulerSharedData
+TypeIndex: 17
+
+TypeName: PsSiloContextPaged
+TypeIndex: 18
+
+TypeName: PsSiloContextNonPaged
+TypeIndex: 19
+
+TypeName: DebugObject
+TypeIndex: 20
+
+TypeName: Event
+TypeIndex: 21
+
+TypeName: Mutant
+TypeIndex: 22
+
+TypeName: Callback
+TypeIndex: 23
+
+TypeName: Semaphore
+TypeIndex: 24
+
+TypeName: Timer
+TypeIndex: 25
+
+TypeName: IRTimer
+TypeIndex: 26
+
+TypeName: Profile
+TypeIndex: 27
+
+TypeName: KeyedEvent
+TypeIndex: 28
+
+TypeName: WindowStation
+TypeIndex: 29
+
+TypeName: Desktop
+TypeIndex: 30
+
+TypeName: Composition
+TypeIndex: 31
+
+TypeName: RawInputManager
+TypeIndex: 32
+
+TypeName: CoreMessaging
+TypeIndex: 33
+
+TypeName: ActivationObject
+TypeIndex: 34
+
+TypeName: TpWorkerFactory
+TypeIndex: 35
+
+TypeName: Adapter
+TypeIndex: 36
+
+TypeName: Controller
+TypeIndex: 37
+
+TypeName: Device
+TypeIndex: 38
+
+TypeName: Driver
+TypeIndex: 39
+
+TypeName: IoCompletion
+TypeIndex: 40
+
+TypeName: WaitCompletionPacket
+TypeIndex: 41
+
+TypeName: File
+TypeIndex: 42
+
+TypeName: IoRing
+TypeIndex: 43
+
+TypeName: TmTm
+TypeIndex: 44
+
+TypeName: TmTx
+TypeIndex: 45
+
+TypeName: TmRm
+TypeIndex: 46
+
+TypeName: TmEn
+TypeIndex: 47
+
+TypeName: ALPC Port
+TypeIndex: 48
+
+TypeName: Section
+TypeIndex: 49
+
+TypeName: Key
+TypeIndex: 50
+
+TypeName: RegistryTransaction
+TypeIndex: 51
+
+TypeName: DmaAdapter
+TypeIndex: 52
+
+TypeName: EnergyTracker
+TypeIndex: 53
+
+TypeName: PowerRequest
+TypeIndex: 54
+
+TypeName: WmiGuid
+TypeIndex: 55
+
+TypeName: EtwRegistration
+TypeIndex: 56
+
+TypeName: EtwSessionDemuxEntry
+TypeIndex: 57
+
+TypeName: EtwConsumer
+TypeIndex: 58
+
+TypeName: CoverageSampler
+TypeIndex: 59
+
+TypeName: PcwObject
+TypeIndex: 60
+
+TypeName: FilterConnectionPort
+TypeIndex: 61
+
+TypeName: FilterCommunicationPort
+TypeIndex: 62
+
+TypeName: DxgkSharedResource
+TypeIndex: 63
+
+TypeName: DxgkSharedKeyedMutexObject
+TypeIndex: 64
+
+TypeName: DxgkSharedSyncObject
+TypeIndex: 65
+
+TypeName: DxgkSharedSwapChainObject
+TypeIndex: 66
+
+TypeName: DxgkDisplayManagerObject
+TypeIndex: 67
+
+TypeName: DxgkSharedProtectedSessionObject
+TypeIndex: 68
+
+TypeName: DxgkSharedBundleObject
+TypeIndex: 69
+
+TypeName: DxgkCompositionObject
+TypeIndex: 70
+
+TypeName: DxgkDisplayMuxSwitch
+TypeIndex: 71
+
+TypeName: NdisCmState
+TypeIndex: 72
+
+TypeName: NdisThreadState
+TypeIndex: 73
+
+TypeName: VRegConfigurationContext
+TypeIndex: 74
